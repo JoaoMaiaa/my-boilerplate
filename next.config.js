3bce965b9c81edb83d4995c72351d1bde8f867f3
João Maia <15joaomaia@gmail.com>
2022-05-03 22:51:28 -0300

    verify eslint

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')
+
+// const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development'
+  }
+})
+
 const nextConfig = {
   reactStrictMode: true
 }
